d10e6584987b838c4dcfa52d62ee4bc43a193618
Jamie Hinton <jamie.hinton@curlyweb.co.uk>
2013-03-22 12:18:35 +0000

    initial commit of extension

---
+++ src/CurlyWeb.ServiceStackCacheExtensions/CurlyWeb.SSCE.Tests/when_storing_to_the_cache.cs
@@ -0,0 +1,39 @@
+﻿namespace CurlyWeb.SSCE.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using NSubstitute;
+  using NUnit.Framework;
+  using ServiceStack.CacheAccess;
+  using ServiceStackCacheExtensions;
+
+  [ TestFixture ]
+  public class When_storing_to_the_cache
+  {
+    [ Test ]
+    public void Should_store_if_not_already_found()
+    {
+      var cacheClient = Substitute.For< ICacheClient >();
+      cacheClient.Get< IList< string > >( "bob" ).Returns( x => null );
+      cacheClient.GetOrSet( "bob", () => new Bob().GetBobs(), TimeSpan.FromHours( 1 ) );
+      cacheClient.Received().Set( Arg.Is( "bob" ), Arg.Any< IList< string > >(), Arg.Any<TimeSpan>() );
+    }
+
+    [ Test ]
+    public void Should_return_from_cache_if_already_there()
+    {
+      var cacheClient = Substitute.For< ICacheClient >();
+      cacheClient.Get< IList< string > >( "bob" ).Returns( x => new List< string >() );
+      cacheClient.GetOrSet( "bob", () => new Bob().GetBobs(), TimeSpan.FromHours( 1 ) );
+      cacheClient.DidNotReceive().Set( Arg.Is( "bob" ), Arg.Any< IList< string > >(), Arg.Any< TimeSpan >() );
+    }
+  }
+
+  internal class Bob
+  {
+    public IList< string > GetBobs()
+    {
+      return new List< string >();
+    }
+  }
+}
