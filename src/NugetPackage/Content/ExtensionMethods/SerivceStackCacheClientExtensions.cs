aea5273156ac84d8505218519d00fe6e61b300b1
Jamie Hinton <jamie.hinton@curlyweb.co.uk>
2013-04-29 21:31:34 +0100

    put the correct file in the package

---
+++ src/NugetPackage/Content/ExtensionMethods/SerivceStackCacheClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace CurlyWeb.ServiceStackCacheExtensions
+{
+  using System;
+  using ServiceStack.CacheAccess;
+
+  public static class CacheClientExtensionMethods
+  {
+    public static T GetOrSet< T >( this ICacheClient cacheClient, string key, Func< T > expression, TimeSpan expiresIn )
+    {
+      var found = cacheClient.Get< T >( key );
+      if( found != null )
+        return found;
+
+      var executed = expression.Invoke();
+      cacheClient.Set( key, executed, expiresIn );
+      return executed;
+    }
+  }
+}
